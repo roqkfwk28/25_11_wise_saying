bf8903fe174bb2cdf4b216e1c76198edd7d73649
roqkfwk28 <roqkfwk0528@gmail.com>
2025-11-24 20:58:02 +0900

    1단계 진행중

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    new App().run();
+  }
+}
+class App{
+  void run() {
+    System.out.println("프로그램 실행");
+    System.out.println("== 명언 앱 ==");
+  }
+}
