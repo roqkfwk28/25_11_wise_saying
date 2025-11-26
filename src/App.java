661c957de6388daa3882b2c6cb36edfaa044e4f5
roqkfwk28 <roqkfwk0528@gmail.com>
2025-11-26 21:41:27 +0900

    5단계 완료

--- src/App.java
+++ src/App.java
@@ -37,7 +37,16 @@ class App {
         System.out.printf("%d번 명언이 등록되었습니다.\n", lastWiseSayingId);
       }
       else if (cmd.equals("목록")) {
-        System.out.println("총 갯수 : " + quotations.size());
+        System.out.println("번호 / 명언 / 작가");
+        System.out.println("--------------------");
+
+        if (quotations.isEmpty()) {
+          System.out.println("등록된 명언이 없습니다.");
+        }
+        for (int i = quotations.size() - 1; i >= 0; i--) {
+          Quotation quotation = quotations.get(i);
+          System.out.printf("%d / %s / %s\n", quotation.id, quotation.content, quotation.authorName);
+        }
       }
     }
   }
