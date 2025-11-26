2f7905ca23436042429fe69011189b8279cb68b3
roqkfwk28 <roqkfwk0528@gmail.com>
2025-11-26 20:25:21 +0900

    Quotation class add

--- src/App.java
+++ src/App.java
@@ -6,23 +6,27 @@ class App {
 
     int lastWiseSayingId = 0;
 
-    while(true){
-      System.out.printf("명령) ");
+    while (true) {
+      System.out.print("명령) ");
 
       Scanner scanner = new Scanner(System.in);
 
       String cmd = scanner.nextLine();
-      if (cmd.equals("종료")){
+      if (cmd.equals("종료")) {
         break;
       }
-      else if(cmd.equals("등록")){
-        System.out.println("명언: ");
+      else if (cmd.equals("등록")) {
+        System.out.print("명언 : ");
         String content = scanner.nextLine();
 
-        System.out.println("작가: ");
+        System.out.print("작가 : ");
         String authorName = scanner.nextLine();
 
         lastWiseSayingId++;
+
+        int id = lastWiseSayingId;
+
+        Quotation quotation = new Quotation(id, content, authorName);
 
         System.out.printf("%d번 명언이 등록되었습니다.\n", lastWiseSayingId);
       }
---
+++ src/Quotation.java
@@ -0,0 +1,11 @@
+public class Quotation {
+  int id ;
+  String content;
+  String authorName;
+
+  public Quotation(int id, String content, String authorName) {
+    this.id = id;
+    this.content = content;
+    this.authorName = authorName;
+  }
+}
