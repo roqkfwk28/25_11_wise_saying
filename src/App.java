2061258fa58481860c89a3f88c3cc6e782d7f020
roqkfwk28 <roqkfwk0528@gmail.com>
2025-11-26 19:42:05 +0900

    스캐너 도입

--- src/App.java
+++ src/App.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 class App {
   void run() {
-    System.out.println("프로그램 실행");
     System.out.println("== 명언 앱 ==");
+    System.out.println("명령) ");
+
+    Scanner scanner = new Scanner(System.in);
+
+    String cmd = scanner.nextLine();
+
+    System.out.printf("입력하신 명령어 : %s\n", cmd);
   }
 }
