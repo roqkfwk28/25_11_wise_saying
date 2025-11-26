dc3d2b4fecb2163fbafbf8b27347a987bb45633d
roqkfwk28 <roqkfwk0528@gmail.com>
2025-11-26 19:59:36 +0900

    등록이 입력되었다면, 특정메세지가 출력

--- src/App.java
+++ src/App.java
@@ -12,6 +12,10 @@ class App {
       if (cmd.equals("종료")){
         break;
       }
+      else if(cmd.equals("등록")){
+        System.out.println("명언: ");
+        System.out.println("작가: ");
+      }
     }
   }
 }
