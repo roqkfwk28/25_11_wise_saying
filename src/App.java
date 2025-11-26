7d77428c4aeb06fc8b96e9ea877ec5c23b90861c
roqkfwk28 <roqkfwk0528@gmail.com>
2025-11-26 19:36:33 +0900

    App 클래스를 별도의 파일로 분리

---
+++ src/App.java
@@ -0,0 +1,6 @@
+class App {
+  void run() {
+    System.out.println("프로그램 실행");
+    System.out.println("== 명언 앱 ==");
+  }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,3 @@ public class Main {
     new App().run();
   }
 }
-class App{
-  void run(){
-    System.out.println("프로그램 실행");
-    System.out.println("== 명언 앱 ==");
-  }
-}
