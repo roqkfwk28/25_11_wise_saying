29ef54233dcab60643527017220d5777adb2350b
roqkfwk28 <roqkfwk0528@gmail.com>
2025-12-01 21:25:25 +0900

    getParamAslnt 함수 도입

--- src/App.java
+++ src/App.java
@@ -3,51 +3,51 @@ import java.util.List;
 import java.util.Scanner;
 
 class App {
-    Scanner scanner;
-    int lastWiseSayingId;
-    List<Quotation> quotations;
+  Scanner scanner;
+  int lastWiseSayingId;
+  List<Quotation> quotations;
 
-    App() {
-      scanner = new Scanner(System.in);
-      lastWiseSayingId = 0;
-      quotations = new ArrayList<>();
-    }
+  App() {
+    scanner = new Scanner(System.in);
+    lastWiseSayingId = 0;
+    quotations = new ArrayList<>();
+  }
 
-    void run() {
-      System.out.println("== 명언 앱 ==");
+  void run() {
+    System.out.println("== 명언 앱 ==");
 
-      while (true) {
-        System.out.print("명령) ");
-        String cmd = scanner.nextLine();
+    while (true) {
+      System.out.print("명령) ");
+      String cmd = scanner.nextLine();
 
-        if (cmd.equals("종료")) {
-          break;
-        }
-        else if (cmd.equals("등록")) {
-          actionWrite();
-          System.out.printf("%d번 명언이 등록되었습니다.\n", lastWiseSayingId);
-        }
-        else if (cmd.equals("목록")) {
-          actionList();
-        }
-        else if (cmd.startsWith("삭제?")){ //cmd에서 삭제? 시작하는가? true면 함수 호출
-          actionRemove(cmd);
-        }
+      if (cmd.equals("종료")) {
+        break;
+      }
+      else if (cmd.equals("등록")) {
+        actionWrite();
+        System.out.printf("%d번 명언이 등록되었습니다.\n", lastWiseSayingId);
+      }
+      else if (cmd.equals("목록")) {
+        actionList();
+      }
+      else if (cmd.startsWith("삭제?")){ //cmd에서 삭제? 시작하는가? true면 함수 호출
+        actionRemove(cmd);
       }
     }
+  }
 
-    void actionWrite() {
-      System.out.print("명언 : ");
-      String content = scanner.nextLine();
+  void actionWrite() {
+    System.out.print("명언 : ");
+    String content = scanner.nextLine();
 
-      System.out.print("작가 : ");
-      String authorName = scanner.nextLine();
+    System.out.print("작가 : ");
+    String authorName = scanner.nextLine();
 
-      lastWiseSayingId++;
+    lastWiseSayingId++;
 
-      int id = lastWiseSayingId;
+    int id = lastWiseSayingId;
 
-      Quotation quotation = new Quotation(id, content, authorName);
+    Quotation quotation = new Quotation(id, content, authorName);
 
     quotations.add(quotation);
   }
@@ -64,7 +64,18 @@ class App {
       System.out.printf("%d / %s / %s\n", quotation.id, quotation.content, quotation.authorName);
     }
   }
+
   void actionRemove(String cmd){//매개변수 cmd에 삭제?id=1 문자열이 들어있다면
+    int id = getParamAsInt(cmd, "id",0);
+    if (id == 0) {
+      System.out.println("id를 정확히 입력해주세요.");
+      return;
+    }
+
+    System.out.printf("%d번 명언을 삭제합니다.\n", id);
+  }
+  int getParamAsInt(String cmd, String paramName, int defaultValue){
+
     String[] cmdBits = cmd.split("\\?", 2);
     String action = cmdBits[0];
     String queryString = cmdBits[1];
@@ -78,14 +89,19 @@ class App {
 
       String[] queryParamStrBits = queryParamStr.split("=", 2);
 
-      String paramName = queryParamStrBits[0];
+      String _paramName = queryParamStrBits[0];
       String paramValue = queryParamStrBits[1];
 
-      if (paramName.equals("id")) {
-        id = Integer.parseInt(paramValue);
+        if (_paramName.equals(paramName)) {
+          try {
+            // 문제가 없을 경우
+            return Integer.parseInt(paramValue);
+          }catch (NumberFormatException e){
+            //문제가 있을 경우
+            return defaultValue;
+          }
+        }
       }
+      return defaultValue;
     }
-
-    System.out.printf("%d번 명언을 삭제합니다.\n", id);
   }
-}
