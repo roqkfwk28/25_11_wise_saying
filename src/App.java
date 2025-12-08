b24ba9d0679d1ff0b909e4132416a5c89bcbe024
roqkfwk28 <roqkfwk0528@gmail.com>
2025-12-08 19:24:17 +0900

    명언 삭제 및 예외처리

--- src/App.java
+++ src/App.java
@@ -51,6 +51,8 @@ class App {
     Quotation quotation = new Quotation(id, content, authorName);
 
     quotations.add(quotation);
+
+    System.out.printf("%d번 명언이 등록 되었습니다.\n", lastWiseSayingId);
   }
 
   void actionList() {
@@ -72,7 +74,22 @@ class App {
       System.out.println("id를 정확히 입력해주세요.");
       return;
     }
-
+    int index = get_index_Of_wise_sayingId(id);
+    if (index == -1) {
+      System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
+      return;
+    }
+    quotations.remove(index);
     System.out.printf("%d번 명언을 삭제합니다.\n", id);
+  }
+  int get_index_Of_wise_sayingId(int id) {
+    for (int i = 0; i <quotations.size(); i++) {
+      Quotation quotation = quotations.get(i);
+
+      if (quotation.id == id) {
+        return i;
+      }
+    }
+    return -1;
   }
 }
