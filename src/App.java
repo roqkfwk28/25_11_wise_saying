9842bd9d032c283ff7dbf66e7a16cdc69377f971
roqkfwk28 <roqkfwk0528@gmail.com>
2025-12-08 19:20:07 +0900

    switch문 적용시 break 누락으로인한 수정

--- src/App.java
+++ src/App.java
@@ -21,55 +21,58 @@ class App {
       String cmd = scanner.nextLine();
 
       Rq rq = new Rq(cmd);
-        switch (rq.getAction()) {
-          case "종료":
-            return;
-          case "등록":
-            actionWrite();
-          case "목록":
-            actionList();
-          case "삭제":
-            actionRemove(rq);
-        }
-      }
-    }
-
-    void actionWrite() {
-      System.out.print("명언 : ");
-      String content = scanner.nextLine();
-
-      System.out.print("작가 : ");
-      String authorName = scanner.nextLine();
-
-      lastWiseSayingId++;
-
-      int id = lastWiseSayingId;
-
-      Quotation quotation = new Quotation(id, content, authorName);
-
-      quotations.add(quotation);
-    }
-
-    void actionList() {
-      System.out.println("번호 / 명언 / 작가");
-      System.out.println("--------------------");
-
-      if (quotations.isEmpty()) {
-        System.out.println("등록된 명언이 없습니다.");
-      }
-      for (int i = quotations.size() - 1; i >= 0; i--) {
-        Quotation quotation = quotations.get(i);
-        System.out.printf("%d / %s / %s\n", quotation.id, quotation.content, quotation.authorName);
-      }
-    }
-
-      void actionRemove(Rq rq) {
-        int id = rq.getParamAsInt("id", 0);
-        if (id == 0) {
-          System.out.println("id를 정확히 입력해주세요.");
+      switch (rq.getAction()) {
+        case "종료":
           return;
-        }
-
-        System.out.printf("%d번 명언을 삭제합니다.\n", id);
+        case "등록":
+          actionWrite();
+          break;
+        case "목록":
+          actionList();
+          break;
+        case "삭제":
+          actionRemove(rq);
+          break;
       }
     }
+  }
+
+  void actionWrite() {
+    System.out.print("명언 : ");
+    String content = scanner.nextLine();
+
+    System.out.print("작가 : ");
+    String authorName = scanner.nextLine();
+
+    lastWiseSayingId++;
+
+    int id = lastWiseSayingId;
+
+    Quotation quotation = new Quotation(id, content, authorName);
+
+    quotations.add(quotation);
+  }
+
+  void actionList() {
+    System.out.println("번호 / 명언 / 작가");
+    System.out.println("--------------------");
+
+    if (quotations.isEmpty()) {
+      System.out.println("등록된 명언이 없습니다.");
+    }
+    for (int i = quotations.size() - 1; i >= 0; i--) {
+      Quotation quotation = quotations.get(i);
+      System.out.printf("%d / %s / %s\n", quotation.id, quotation.content, quotation.authorName);
+    }
+  }
+
+  void actionRemove(Rq rq) {
+    int id = rq.getParamAsInt("id", 0);
+    if (id == 0) {
+      System.out.println("id를 정확히 입력해주세요.");
+      return;
+    }
+
+    System.out.printf("%d번 명언을 삭제합니다.\n", id);
+  }
+}
